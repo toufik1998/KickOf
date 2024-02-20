2ad46fb1f08dbf88697cb0da1e846d65b456268e
toufik1998 <toufikshima98@gmail.com>
2024-02-20 08:57:32 +0100

    initialize and create the project architecture

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native'; 
+import { createNativeStackNavigator } from '@react-navigation/native-stack'; 
+import HomeScreen from './src/HomeScreen/HomeScreen';
 
 export default function App() {
+
+  const Stack = createNativeStackNavigator();
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Signin">
+        <Stack.Screen name="Home" />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ src/HomeScreen/HomeScreen.js
@@ -0,0 +1,15 @@
+import { View, Text, StyleSheet, ScrollView, TouchableOpacity, Linking } from 'react-native'
+
+function HomeScreen({naviation}) {
+    return (
+        <View>
+          <View>
+          <Text>Find The pharmacy of guard </Text>
+          <Text>any time </Text>
+          <Text>where !</Text>
+        </View>
+        </View>
+    )
+}
+
+export default HomeScreen
